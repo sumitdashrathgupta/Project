66dce009931fbf2b6c3517108c9fcafc3014c357
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-05 16:35:47 +0530

    Update

--- E-Commerce/src/Pages/Navbar.jsx
+++ E-Commerce/src/Pages/Navbar.jsx
@@ -1,24 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+import logo from "../../public/Image/Logo.png"
+
 const Navbar = () => {
   return (
-    <header className="w-full h-full bg-amber-400">
-      <nav>
+    <header className="w-full h-full text-cyan-50 ">
+      <nav className="w-1/1  mx-auto align-middle justify-between flex px-35 py-2 bg-amber-700">
         <div className="">
-          <h1 className="font-bold">Watch</h1>
+          <img src={logo} className="w-[80px]" alt="" />
         </div>
-        <ul className=" bg-amber-950">
-          <li>
+        <ul className="flex align-middle justify-center gap-5 ">
+          <li className="hover:text-blue-300">
             <NavLink to={"/"}>Home</NavLink>
           </li>
-          <li>
+          <li className="hover:text-blue-300">
             <NavLink to={"/products"}>Product</NavLink>
           </li>
-          <li>
+          <li className="hover:text-blue-300">
             <NavLink to={"/services"}>Service</NavLink>
           </li>
-          <li>
+          <li className="hover:text-blue-300">
             <NavLink to={"/about"}>About</NavLink>
+          </li>
+          <li className="flex align-middle justify-center">
+            <NavLink to={"/cart"} >
+              <FaShoppingCart />
+            </NavLink>
           </li>
         </ul>
       </nav>
