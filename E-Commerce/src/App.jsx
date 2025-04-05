32d2e73cadafec3e101f6e584ecee26b96ebc785
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-05 15:16:58 +0530

    Update navbar

--- E-Commerce/src/App.jsx
+++ E-Commerce/src/App.jsx
@@ -1,10 +1,22 @@
 import React from "react";
 import Home from "./Pages/Home";
+import { Route, Routes } from "react-router-dom";
+import About from "./Pages/About";
+import Service from "./Pages/Service";
+import Product from "./Pages/Product";
+import Navbar from "./Pages/Navbar"
+
 
 const App = () => {
   return (
     <>
-      <Home />
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/services" element={<Service />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </>
   );
 };
---
+++ E-Commerce/src/Pages/Item.jsx
@@ -0,0 +1,63 @@
+import Image1 from "../../public/Image/W-1.jpg";
+import Image2 from "../../public/Image/W-2.jpg";
+import Image3 from "../../public/Image/W-3.jpg";
+import Image4 from "../../public/Image/W-4.jpg";
+import Image5 from "../../public/Image/W-5.jpg";
+import Image6 from "../../public/Image/W-6.jpg";
+import Image7 from "../../public/Image/W-7.jpg";
+import Image8 from "../../public/Image/W-8.jpg";
+import Image9 from "../../public/Image/W-9.jpg";
+import Image10 from "../../public/Image/W-10.jpg";
+
+export const Item = [
+  {
+    Image: { Image1 },
+    Title: "Watch 1",
+    Price: "₹ 4999",
+  },
+  {
+    Image: { Image2 },
+    Title: "Watch 2",
+    Price: "₹ 3999",
+  },
+  {
+    Image: { Image3 },
+    Title: "Watch 3",
+    Price: "₹ 5999",
+  },
+  {
+    Image: { Image4 },
+    Title: "Watch 4",
+    Price: "₹ 2999",
+  },
+  {
+    Image: { Image5 },
+    Title: "Watch 5",
+    Price: "₹ 6999",
+  },
+  {
+    Image: { Image6 },
+    Title: "Watch 6",
+    Price: "₹ 5999",
+  },
+  {
+    Image: { Image7 },
+    Title: "Watch 7",
+    Price: "₹ 3999",
+  },
+  {
+    Image: { Image8 },
+    Title: "Watch 8",
+    Price: "₹ 2999",
+  },
+  {
+    Image: { Image9 },
+    Title: "Watch 9",
+    Price: "₹ 1999",
+  },
+  {
+    Image: { Image10 },
+    Title: "Watch 10",
+    Price: "₹ 7999",
+  },
+];
