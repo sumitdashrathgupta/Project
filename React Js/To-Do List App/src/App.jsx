6c8ab16ed11bf00a48cfe36b78645663010585c3
Sumit Gupta <mr.sumitdashrathgupta@gmail.com>
2025-04-13 12:19:46 +0530

    update

--- React Js/To-Do List App/src/App.jsx
+++ React Js/To-Do List App/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import Data from "./Data";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [data, setData] = useState("");
+
+
+  const submitEvent = (e) => {
+    e.preventDefault();
+    console.log(data);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+        <div className="input_data">
+          <form onSubmit={submitEvent}>
+            <input
+              type="text"
+              placeholder="Enter the Data"
+              name="text"
+              id="text"
+            />
+            <button className="btn">Add</button>
+          </form>
+        </div>
+        <div className="Data">
+          <Data text={data} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
